1ebe4c466498d9c40e4e3c045a080cf10454600a
AxialLP7 <axial.jc@gmail.com>
2015-03-06 10:17:37 -0800

    Initial Commit

---
+++ Language Scanner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Language Scanner
+//
+//  Created by Jer Cherng Law on 3/6/15.
+//  Copyright (c) 2015 Jer Cherng Law. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
